86dc613c12d2a77b9d7dd2e10c6b638384f396f7
jorgelina schaf <jor.schaf@gmail.com>
2022-12-11 19:52:00 -0300

    Proyecto final

---
+++ script/script.js
@@ -0,0 +1,11 @@
+function salir(){
+    var respuesta = confirm('¿Seguro que desea salir de esta página?');
+    if (respuesta == true)
+    {
+        return true;
+    }
+    else
+    {
+     return false;
+    }
+}
